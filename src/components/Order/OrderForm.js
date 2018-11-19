6aa560eb407d7448904c3b8b36156ce29b9d3d45
Dwaxy <joel.innes@gmail.com>
2018-11-20 11:16:29 +1300

    Update OrderForm.js

--- src/components/Order/OrderForm.js
+++ src/components/Order/OrderForm.js
@@ -18,12 +18,11 @@ export default class OrderForm extends Component {
         lable: "New Zealand", 
         value: "NZ",
       },
-      item_title: props.orderData[0].frontmatter.title,
-      item_url: props.orderData[0].fields.slug,
+      // item_title: this.props.orderData[0].frontmatter.title,
+      // item_url: this.props.orderData[0].fields.slug,
       submitMsg: "Details will be used to ship the items to you.",
     }
-    console.log(this.props)
-    //this.setOrderDetails()
+    this.setOrderDetails()
   }
   
   // setOrderDetails = () => {
@@ -34,10 +33,10 @@ export default class OrderForm extends Component {
 
   // }
   
-  // setOrderDetails = () => {
-  //   this.props.orderData.map(i => { this.setState({"title": i.frontmatter.title, "url": i.fields.slug}) })
-  //   console.log(this.state)
-  // }
+  setOrderDetails = () => {
+    this.props.orderData.map(i => { this.setState({"title": i.frontmatter.title, "url": i.fields.slug}) })
+    console.log(this.state)
+  }
 
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value})
