41e82a5801d5f9287a15d81318f37e144460f04d
Dwaxy <joel.innes@gmail.com>
2019-01-08 15:15:43 +1300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -151,54 +151,57 @@ exports.createPages = ({ actions, graphql }) => {
     //     },
     //   })
     // })
+    
+    
+    // deprecated !!!!!!
 
-   //  // Tag pages:
-   //  let tags = []
-   //  // Iterate through each post, putting all found tags into `tags`
-   //  allStorePosts.forEach(edge => {
-   //    if (_.get(edge, `node.frontmatter.tags`)) {
-   //      tags = tags.concat(edge.node.frontmatter.tags)
-   //    }
-   //  })
-   //  // Eliminate duplicate tags
-   //  tags = _.uniq(tags)
+    // Tag pages:
+    let tags = []
+    // Iterate through each post, putting all found tags into `tags`
+    allStorePosts.forEach(edge => {
+      if (_.get(edge, `node.frontmatter.tags`)) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+    // Eliminate duplicate tags
+    tags = _.uniq(tags)
 
-   //  // Make tag pages
-   //  tags.forEach(tag => {
-   //    const tagPath = `/cat/${_.kebabCase(tag)}/`
+    // Make tag pages
+    tags.forEach(tag => {
+      const tagPath = `/cat/${_.kebabCase(tag)}/`
 
-   //    createPage({
-   //      path: tagPath,
-   //      component: path.resolve(`src/templates/tags.js`),
-   //      context: {
-   //        tag,
-   //      },
-   //    })
-   //  })
+      createPage({
+        path: tagPath,
+        component: path.resolve(`src/templates/tags.js`),
+        context: {
+          tag,
+        },
+      })
+    })
   
   
-   // // Tag pages:
-   //  let types = []
-   //  // Iterate through each post, putting all found tags into `tags`
-   //  allStorePosts.forEach(edge => {
-   //    if (_.get(edge, `node.frontmatter.type`)) {
-   //      types = types.concat(edge.node.frontmatter.type)
-   //    }
-   //  })
-   //  // Eliminate duplicate tags
-   //  types = _.uniq(types)
-   //  // Make tag pages
-   //  types.forEach(type => {
-   //    const typePath = `/mediums/${_.kebabCase(type)}/`
+   // Tag pages:
+    let types = []
+    // Iterate through each post, putting all found tags into `tags`
+    allStorePosts.forEach(edge => {
+      if (_.get(edge, `node.frontmatter.type`)) {
+        types = types.concat(edge.node.frontmatter.type)
+      }
+    })
+    // Eliminate duplicate tags
+    types = _.uniq(types)
+    // Make tag pages
+    types.forEach(type => {
+      const typePath = `/mediums/${_.kebabCase(type)}/`
 
-   //    createPage({
-   //      path: typePath,
-   //      component: path.resolve(`src/templates/mediums.js`),
-   //      context: {
-   //        type,
-   //      },
-   //    })
-   //  })
+      createPage({
+        path: typePath,
+        component: path.resolve(`src/templates/mediums.js`),
+        context: {
+          type,
+        },
+      })
+    })
     
   })
 }
