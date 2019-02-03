d4626c832732c4d144d9d54fb7ab3bc80f18b61a
Dwaxy <joel.innes@gmail.com>
2019-02-03 17:06:28 +1300

    tweaks

--- src/templates/mediums.js
+++ src/templates/mediums.js
@@ -13,7 +13,7 @@ export class Mediums extends Component {
       allTitles.push(post.node.frontmatter.title)
     })
     return (
-      <div>
+      <div style={{position: 'absolute', top: 0, height: '100%', width: '100%'}}>
         <HeaderMeta pageTitle={this.props.pageContext.medium} itemGroup={allTitles}/>
                  
         <Header to={["home", "archive"]} white={true} />      
