c0933f3d8f23c6a1fb514ad851c40a99166b691f
Dwaxy <joel.innes@gmail.com>
2019-02-19 15:55:20 +1300

    landing tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,39 +18,14 @@ import './mixins.module.scss'
 const ArchiveSlide = (props) => {
   const heading = `Explore a world`
   const desc = `An Archive of lore, illustrations, and short stories.`
-  const archiveItem = {
-    title: `title`,
-    text:
-      `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-
-    link: `/archive/rising-phoenix`
-  }
   return (
     <div className={S.slideOne}>
       <Img fluid={props.img} className={S.heroImage}/>
       <div className={S.holder}>
         <h2 className={S.heading}>{heading}</h2>
         <h3 className={S.desc}>{desc}</h3>
-        
-        <div className={S.archiveItemHolder}>
-          <div className={S.archiveItem}>
-            <h3>{archiveItem.title}</h3>
-            <p>{archiveItem.text}</p>
-          </div>
           
-          <Link to={archiveItem.link} > View In Archive </Link>
-        </div>
+        <Link to="/archive/" > View In Archive </Link>
         
       </div>
     </div>
