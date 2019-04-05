b1407843301f432ce631db52197f8472d9717613
Dwaxy <joel.innes@gmail.com>
2019-04-05 16:16:08 +1300

    npm updates

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,6 +18,12 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   }
 }
 
+// const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+
+// exports.onCreateNode = ({ node }) => {
+//   fmImagesToRelative(node);
+// };
+
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
 
