75fa480a7feb00aa6af8b19f32ec653d447ff54b
Joel <joel.innes@gmail.com>
2020-09-20 12:03:15 +1200

    Test netlify build with GridTemplate in med and cat components

--- src/components/gridTemplate/gridTemplate.js
+++ src/components/gridTemplate/gridTemplate.js
@@ -1,70 +1,70 @@
 
-import React, { Component } from 'react';
-import S from './imageGrid.module.sass'
-import ArtImage from '../ArtImgae/ArtImage.js'
-import Link from 'gatsby-link'
-import { arrowSvg } from '../../img/svg-index.js'
-import InlineSVG from 'svg-inline-react'
-import Header from '../Header/Header.js'
+// import React, { Component } from 'react';
+// import S from './imageGrid.module.sass'
+// import ArtImage from '../ArtImgae/ArtImage.js'
+// import Link from 'gatsby-link'
+// import { arrowSvg } from '../../img/svg-index.js'
+// import InlineSVG from 'svg-inline-react'
+// import Header from '../Header/Header.js'
 
-import 'typeface-alegreya-sans-sc'
-import 'typeface-cinzel-decorative'
-import 'typeface-cinzel'
+// import 'typeface-alegreya-sans-sc'
+// import 'typeface-cinzel-decorative'
+// import 'typeface-cinzel'
 
-/** 
- * @param {} data an object of art item nodes to display in a grid. Frontmatter requied 
- * @param {} title a title of the page
-*/
+// /** 
+//  * @param {} data an object of art item nodes to display in a grid. Frontmatter requied 
+//  * @param {} title a title of the page
+// */
 
-class GridTemplate extends Component {
+// class GridTemplate extends Component {
   
-  render() {
-    const postLinks = this.props.data.map( post => {
-      const frontmatter = post.node.frontmatter
-      return (
-        <div key={post.node.fields.slug} className={S.imageItem}>
-          <Link 
-            to={post.node.fields.slug}
-            //pass prop of cat / med paths for back button on art item 
-            state={{pastUrl: this.props.pastUrl || null}}  
-          >
-            <h2>{frontmatter.title}</h2>
-            <ArtImage
-              fluid={frontmatter.featuredImage.childImageSharp.fluid} 
-              imageData={frontmatter}
-            />
-          </Link>
-        </div>
-      )
-    })
+//   render() {
+//     const postLinks = this.props.data.map( post => {
+//       const frontmatter = post.node.frontmatter
+//       return (
+//         <div key={post.node.fields.slug} className={S.imageItem}>
+//           <Link 
+//             to={post.node.fields.slug}
+//             //pass prop of cat / med paths for back button on art item 
+//             state={{pastUrl: this.props.pastUrl || null}}  
+//           >
+//             <h2>{frontmatter.title}</h2>
+//             <ArtImage
+//               fluid={frontmatter.featuredImage.childImageSharp.fluid} 
+//               imageData={frontmatter}
+//             />
+//           </Link>
+//         </div>
+//       )
+//     })
   
     
-    //from context
-    const title = this.props.title
+//     //from context
+//     const title = this.props.title
     
-    //const totalCount = this.props.data.allMarkdownRemark.totalCount not used
+//     //const totalCount = this.props.data.allMarkdownRemark.totalCount not used
     
-    return (
-      <section id={S.GridTemplate}>
+//     return (
+//       <section id={S.GridTemplate}>
       
-        <div className={S.headerHolder}>
-          <Header to={["home", "archive"]} white={true} />
-        </div>
+//         <div className={S.headerHolder}>
+//           <Header to={["home", "archive"]} white={true} />
+//         </div>
       
-        <div className={S.titleHolder}>
-          <Link to = "/store" className={S.storeLink} >
-            <InlineSVG src={arrowSvg} />
-          </Link>  
+//         <div className={S.titleHolder}>
+//           <Link to = "/store" className={S.storeLink} >
+//             <InlineSVG src={arrowSvg} />
+//           </Link>  
           
-          <h1 id={S.mediumTitle}>{title}</h1>
-        </div>
-        <div className={S.imageGrid}>
-          {postLinks}
-        </div>
+//           <h1 id={S.mediumTitle}>{title}</h1>
+//         </div>
+//         <div className={S.imageGrid}>
+//           {postLinks}
+//         </div>
       
-      </section>
-    )
-  }
-}
+//       </section>
+//     )
+//   }
+// }
 
-export default GridTemplate
+// export default GridTemplate
--- src/templates/categorys.js
+++ src/templates/categorys.js
@@ -1,7 +1,75 @@
 import React, { Component } from 'react'
-import GridTemplate from '../components/GridTemplate/GridTemplate'
+// import GridTemplate from '../components/GridTemplate/GridTemplate'
 import { graphql } from 'gatsby'
 import HeaderMeta from '../components/Helmet/Helmet.js'
+
+import S from '../components/GridTemplate/imageGrid.module.sass'
+import ArtImage from '../components/ArtImgae/ArtImage.js'
+import Link from 'gatsby-link'
+import { arrowSvg } from '../img/svg-index.js'
+import InlineSVG from 'svg-inline-react'
+import Header from '../components/Header/Header.js'
+
+import 'typeface-alegreya-sans-sc'
+import 'typeface-cinzel-decorative'
+import 'typeface-cinzel'
+
+/** 
+ * @param {} data an object of art item nodes to display in a grid. Frontmatter requied 
+ * @param {} title a title of the page
+*/
+
+class GridTemplate extends Component {
+  
+  render() {
+    const postLinks = this.props.data.map( post => {
+      const frontmatter = post.node.frontmatter
+      return (
+        <div key={post.node.fields.slug} className={S.imageItem}>
+          <Link 
+            to={post.node.fields.slug}
+            //pass prop of cat / med paths for back button on art item 
+            state={{pastUrl: this.props.pastUrl || null}}  
+          >
+            <h2>{frontmatter.title}</h2>
+            <ArtImage
+              fluid={frontmatter.featuredImage.childImageSharp.fluid} 
+              imageData={frontmatter}
+            />
+          </Link>
+        </div>
+      )
+    })
+  
+    
+    //from context
+    const title = this.props.title
+    
+    //const totalCount = this.props.data.allMarkdownRemark.totalCount not used
+    
+    return (
+      <section id={S.GridTemplate}>
+      
+        <div className={S.headerHolder}>
+          <Header to={["home", "archive"]} white={true} />
+        </div>
+      
+        <div className={S.titleHolder}>
+          <Link to = "/store" className={S.storeLink} >
+            <InlineSVG src={arrowSvg} />
+          </Link>  
+          
+          <h1 id={S.mediumTitle}>{title}</h1>
+        </div>
+        <div className={S.imageGrid}>
+          {postLinks}
+        </div>
+      
+      </section>
+    )
+  }
+}
+
 
 class Category extends Component {
   render() {
